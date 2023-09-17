8f1cc063416c4b756845543395e028977e79ae47
Roman Roux <roman_roux@icloud.com>
2023-09-17 14:15:31 +0200

    Initial Commit

---
+++ Fractal/FractalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FractalApp.swift
+//  Fractal
+//
+//  Created by Roman Roux on 17/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct FractalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
